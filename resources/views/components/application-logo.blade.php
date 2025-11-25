6a15ea1a3058ca7578dec0c3b79c72613605cabf
mariano martin velasco <mariano18martin@gmail.com>
2025-11-25 20:33:28 +0100

    proyecto devjobs

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,3 @@
+<h1 class="text-3xl">
+    Dev<spam class="font-extrabold">Jobs</spam>
+</h1>
---
+++ resources/views/components/link.blade.php
@@ -0,0 +1,3 @@
+<a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+    {{$slot}}
+</a>
