f8f3a23c51feaa00b92a5966dd19d5681bff8ef1
mariano martin velasco <mariano18martin@gmail.com>
2026-01-25 13:08:22 +0100

    Mostrar vacante en el Front End

--- app/Models/Vacante.php
+++ app/Models/Vacante.php
@@ -20,4 +20,13 @@ class Vacante extends Model
         'imagen',
         'user_id',
     ];
+
+
+    public function categoria(){
+        return $this->belongsTo(Categoria::class);
+    }
+
+    public function salario(){
+        return $this->belongsTo(Salario::class);
+    }
 }
