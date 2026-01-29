5e9329fd3866e87bccdd33d4c33edaa44175716c
mariano martin velasco <mariano18martin@gmail.com>
2026-01-29 18:37:05 +0100

    Listando candidatos interesados en una vacante

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -10,7 +10,9 @@
                 <p class="text-sm text-gray-500">Ultimo dia: {{$vacante->ultimo_dia}}</p>
             </div>
             <div class="flex flex-col md:flex-row items-stretch gap-3  mt-5 md:mt-0">
-                <a href="#" class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Candidatos</a>
+                <a href="{{route('candidatos.index', $vacante)}}" class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                    {{$vacante->candidatos->count()}}
+                    Candidatos</a>
                 <a href="{{route('vacantes.edit',$vacante->id)}}" class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Editar</a>
                 <button wire:click="$dispatch('mostrarAlerta', {{$vacante->id}})"
                  class="bg-red-600  py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">Eliminar</button>
